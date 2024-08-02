4ec6f23902330fbd1b6063260aac8a329b48b999
Noah do Rego <ndore051@uottawa.ca>
2024-08-01 21:37:11 -0400

    Fixed add patient cancellation

--- frontend/src/components/AdminNavbar.js
+++ frontend/src/components/AdminNavbar.js
@@ -11,45 +11,52 @@ export function AdminNavbar() {
 
     // Handles adding a patient through prompt inputs
     const addPatient = () => {
+        let name, severity, wait_time = null;
         const severity_options = ["low", "medium", "high", "critical"]
-        const name = prompt("Enter patient name");
-        const severity = prompt("Enter seveity (low/medium/high/critical): ");
-        const wait_time = prompt("Enter wait time (in minutes): ");
-        if (severity_options.includes(severity) && !isNaN(parseFloat(wait_time))) {
-            fetch(`http://localhost:8000/api/patients`, {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ name: name, severity: severity, wait_time: wait_time })
-            })
-                .then(response => response.json())
-                .then(data => {
-                    fetch(`http://localhost:8000/api/admin/wait-times`, {
-                        method: "PUT",
-                        mode: "cors",
-                        headers: {
-                            'Authorization': 'Basic ' + admin_key,
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            'critical': 5,
-                            'high': 10,
-                            'medium': 15,
-                            'low': 20
-                        }),
-                    })
-                        .then(response => response.json())
-                        .then(data => {
-                            console.log(data);
-                            navigate(0);
-                        })
-                        .catch(error => console.error('Error:', error));
+        name = prompt("Enter patient name");
+        if (name != null) {
+            severity = prompt("Enter seveity (low/medium/high/critical): ");
+        }
+        if (severity != null) {
+            wait_time = prompt("Enter wait time (in minutes): ");
+        }
+        if (name != null && severity != null && wait_time != null) {
+            if (severity_options.includes(severity) && !isNaN(parseFloat(wait_time))) {
+                fetch(`http://localhost:8000/api/patients`, {
+                    method: "POST",
+                    mode: "cors",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ name: name, severity: severity, wait_time: wait_time })
                 })
-                .catch(error => console.error('Error:', error));
-        } else {
-            alert("Invalid values.");
+                    .then(response => response.json())
+                    .then(data => {
+                        fetch(`http://localhost:8000/api/admin/wait-times`, {
+                            method: "PUT",
+                            mode: "cors",
+                            headers: {
+                                'Authorization': 'Basic ' + admin_key,
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                'critical': 5,
+                                'high': 10,
+                                'medium': 15,
+                                'low': 20
+                            }),
+                        })
+                            .then(response => response.json())
+                            .then(data => {
+                                console.log(data);
+                                navigate(0);
+                            })
+                            .catch(error => console.error('Error:', error));
+                    })
+                    .catch(error => console.error('Error:', error));
+            } else {
+                alert("Invalid values.");
+            }
         }
     }
 
